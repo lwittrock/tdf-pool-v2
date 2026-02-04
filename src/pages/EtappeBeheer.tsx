7191f18e68f9c79878e949fe0b68ede601d7158d
Lars Wittrock <larwitt@gmx.de>
2026-02-04 21:38:53 +0100

    Undo

--- src/pages/EtappeBeheer.tsx
+++ src/pages/EtappeBeheer.tsx
@@ -17,7 +17,6 @@ import { useRefreshTdfData } from '../hooks/useRefreshTdfData';
 import { useStagesData, useRiders } from '../hooks/useTdfData';
 import { JERSEY_ICONS } from '../../lib/constants';
 import type { StageData, RidersData } from '../../lib/types';
-import { TestDataSeeder } from '../components/TestDataSeeder';
 
 // ============================================================================
 // Types
@@ -304,7 +303,7 @@ function StageManagementPage() {
             onOpenEntry={handleOpenEntry}
           />
         )}
-        <TestDataSeeder />
+
         {/* VIEW MODE */}
         {viewMode === 'view' && (
           <StageViewMode
